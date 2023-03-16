f0ad7b63766bf124954ce4489a0546dbc7b5ad91
Santos, B <beasantosdev@gmail.com>
2023-03-16 14:14:44 -0300

    feature(backend): Add calculation business rules
    SUMMARY:
    backend
     - operations: Added providers, sevices and related business rules and its tests
     - records: same as operations

--- backend/operations/tests/test_providers.py
+++ backend/operations/tests/test_providers.py
@@ -26,3 +26,34 @@ class GetOperationByTypeTestCase(TestCase):
         received_operation = operations_providers.get_operation_by_type(_type=fake_type)
         
         self.assertIsNone(received_operation)
+class FilterOperationByTypeTestCase(TestCase):
+    def test_existent_single_operation(self):
+        for _type in list(zip(*TYPE_CHOICES))[0][:-1]:
+            with self.subTest(_type=_type):
+                operation_obj = operations_factories.OperationFactory(type=_type)
+                operation_obj.save()
+                
+                expected_operation_id = operation_obj.id
+                expected_operation_cost = operation_obj.cost
+                expected_count = 1
+                
+                
+                received_op_qs = operations_providers.filter_operation_by_type(operation_types=[_type])
+                self.assertEqual(expected_count, received_op_qs.count())
+                received_operation = received_op_qs.first()
+                self.assertEqual(expected_operation_cost, received_operation.cost)
+                self.assertEqual(expected_operation_id, received_operation.id)
+    def test_existent_multiple_operations(self):
+        types = list(zip(*TYPE_CHOICES))[0][:-1]
+        expected_length = len(types)
+        operation_objs = [operations_factories.OperationFactory(type=_type) for _type in types]
+        
+        
+        received_op_qs = operations_providers.filter_operation_by_type(operation_types=types)
+        self.assertEqual(expected_length, received_op_qs.count())
+        
+    def test_non_existent_operation(self):
+        fake_type= ["FFF"]
+        received_operation = operations_providers.filter_operation_by_type(operation_types=fake_type)
+        
+        self.assertIsNone(received_operation)
--- backend/records/tests/test_business_rules.py
+++ backend/records/tests/test_business_rules.py
@@ -3,7 +3,11 @@ from unittest import TestCase
 from records.tests import factories as records_factories
 from records.dataclasses import RecordData
 from datetime import datetime
+from records.constants import OK_REASON, INSUFICIENT_FUNDS_REASON,USER_DOES_NOT_EXIST_REASON
 from uuid import uuid4
+from operations.tests.factories import OperationFactory
+from operations.constants import TYPE_CHOICES
+from random import sample, randint, choices
 
 class GetLastUserBalancePerUserIdtestCase(TestCase):
     def test_get_existent_user_balance(self):
@@ -36,4 +40,76 @@ class GetLastUserBalancePerUserIdtestCase(TestCase):
         
         self.assertIsNone(received_user_balance)
         
+class IsBalanceForOperationEnoughTestCase(TestCase):
+    def setUp(self):
+        self.user_balance = 1000000
+        last_record = records_factories.RecordFactory(user_balance=1000000)
+        self.user_id = last_record.user_id
+    def test_user_has_suficient_funds(self):
+        cost=100000
+        received_return = records_business_rules.is_balance_for_operation_enough(cost=cost, user_id=self.user_id)
+        self.assertTrue(received_return)
+    def test_user_has_insuficient_funds(self):
+        cost=10000000
+        received_return = records_business_rules.is_balance_for_operation_enough(cost=cost, user_id=self.user_id)
+        self.assertFalse(received_return)
+    def test_user_has_no_record(self):
+        cost=10000000
+        received_return = records_business_rules.is_balance_for_operation_enough(cost=cost, user_id=uuid4())
+        self.assertIsNone(received_return)
     
+class GenerateReasontestCase(TestCase):
+    def test_user_dont_exist(self):
+        authorized = None
+        reason = USER_DOES_NOT_EXIST_REASON
+        expected_return = {
+        "authorized":authorized,
+        "reason": reason
+        }
+        received_return = records_business_rules.generate_response(authorized=authorized)
+        self.assertDictEqual(expected_return,received_return)
+        
+    def test_user_insuficient_funds(self):
+        authorized = False
+        reason = INSUFICIENT_FUNDS_REASON
+        expected_return = {
+        "authorized":authorized,
+        "reason": reason
+        }
+        received_return = records_business_rules.generate_response(authorized=authorized)
+        self.assertDictEqual(expected_return,received_return)
+        
+    def test_user_ok(self):
+        authorized = True
+        reason = OK_REASON
+        expected_return = {
+        "authorized":authorized,
+        "reason": reason
+        }
+        received_return = records_business_rules.generate_response(authorized=authorized)
+        self.assertDictEqual(expected_return,received_return)
+        
+        
+class TotalCostTestCase(TestCase):
+    def setUp(self):
+        all_types =  list(zip(*TYPE_CHOICES))[0]
+        self.operation_objs = [OperationFactory(type=_type) for _type in all_types]
+    
+    def test_single_operation_cost(self):
+        for operation in self.operation_objs:
+            with self.subTest(operation=operation):
+                expected_value = operation.cost
+                received_return = records_business_rules.total_cost(operation_type_list=[operation.type])
+                
+                self.assertEqual(expected_value, received_return)
+                
+    def test_multiple_operations(self):
+        operations_t = [choices(self.operation_objs, k=randint(2,40)) for i in range(randint(2,10))]
+        for operations in operations_t:
+            with self.subTest(operations=operations):
+                expected_operations_cost = sum([operation.cost for operation in operations])
+                operation_type_list = [operation.type for operation in operations]
+                received_return = records_business_rules.total_cost(operation_type_list=operation_type_list)
+                self.assertEqual(expected_operations_cost, received_return)
+            
+            
